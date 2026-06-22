5522322f301f95ca7aec2ddcadd6a01f300806e6
MuseoZannatoRob <roberto.battiston@comune.montecchio-maggiore.vi.it>
2026-06-22 16:53:08 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,61 @@
-
 const TEXT={
-en:{welcome:"Human interpretation of historical museum labels",cont:"Continue",start:"Start Challenge",profile:"Participant Profile"},
-it:{welcome:"Interpretazione umana di etichette museali storiche",cont:"Continua",start:"Inizia la sfida",profile:"Profilo partecipante"}
+
+en:{
+welcome:"Human interpretation of historical museum labels",
+cont:"Continue",
+start:"Start Challenge",
+profile:"Participant Profile",
+
+age:"Age",
+language:"Native language",
+expertise:"Expertise",
+
+age_u18:"<18",
+age_18_30:"18–30",
+age_31_50:"31–50",
+age_51_65:"51–65",
+age_o65:">65",
+
+exp_none:"None",
+exp_amateur:"Amateur naturalist",
+exp_entomologist:"Entomologist",
+exp_curator:"Museum curator",
+
+results:"Results",
+accuracy:"Accuracy",
+avgtime:"Average Time",
+speed:"Speed Score",
+final:"Final Score",
+saved:"Rows saved"
+},
+
+it:{
+welcome:"Interpretazione umana di etichette museali storiche",
+cont:"Continua",
+start:"Inizia la sfida",
+profile:"Profilo partecipante",
+
+age:"Età",
+language:"Lingua madre",
+expertise:"Competenza",
+
+age_u18:"<18",
+age_18_30:"18–30",
+age_31_50:"31–50",
+age_51_65:"51–65",
+age_o65:">65",
+
+exp_none:"Nessuna",
+exp_amateur:"Naturalista amatoriale",
+exp_entomologist:"Entomologo",
+exp_curator:"Curatore museale",
+
+results:"Risultati",
+accuracy:"Accuratezza",
+avgtime:"Tempo medio",
+speed:"Punteggio velocità",
+final:"Punteggio finale",
+saved:"Righe salvate"
+}
+
 };
