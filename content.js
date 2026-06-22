d5b3acba8d5f7f7ebed53ddb0cca81abe2b805a1
MuseoZannatoRob <roberto.battiston@comune.montecchio-maggiore.vi.it>
2026-06-22 14:54:16 +0200

    Add files via upload

---
+++ content.js
@@ -0,0 +1,5 @@
+
+const TEXT={
+en:{welcome:"Human interpretation of historical museum labels",cont:"Continue",start:"Start Challenge",profile:"Participant Profile"},
+it:{welcome:"Interpretazione umana di etichette museali storiche",cont:"Continua",start:"Inizia la sfida",profile:"Profilo partecipante"}
+};
